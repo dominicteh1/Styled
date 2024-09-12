d26f27264faa87e378d001917fe729b495be5fb8
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2024-09-11 23:01:56 -0400

    Initial Commit

---
+++ Style/StyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleApp.swift
+//  Style
+//
+//  Created by Dominic Teh on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
