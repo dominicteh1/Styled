ddf297a15ff507fa298d5ab3aaf263da5dab5718
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2024-09-16 21:42:53 -0400

    Renamed Build plist data from "Style" to "Styled" so that build previews work

---
+++ Styled/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Style
+//
+//  Created by Dominic Teh on 11/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Test commit")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
